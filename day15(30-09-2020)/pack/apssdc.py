883a02e025dc1c3d5f4691d0ac132c9db8b2c176
yamini <yaminivasa1524@gmail.com>
2020-11-08 16:27:48 +0530

    first commit

---
+++ day15(30-09-2020)/pack/apssdc.py
@@ -0,0 +1,11 @@
+def even(a):
+    if a%2==0:
+        print("even")
+    else:
+        print("odd")
+def largest(a,b):
+    if a>b:
+        print(a)
+    else:
+        print(b)
+        
---
+++ day2(15-09-2020)/sample4.py
@@ -0,0 +1,34 @@
+print("Hello my name is yamini")
+
+a=b=c=170
+
+s=10;
+y=20;
+s,y=y,s
+
+
+
+a=10
+b=20
+print("before swapping:",a,b);
+a=a+b
+b=a-b
+a=a-b
+print("after swapping:",a,b);
+
+
+
+a=input("enter a avalue:");
+print(type(a))
+
+
+
+b=float("enter a value:");
+print(type(b))
+
+
+
+        
+
+
+
